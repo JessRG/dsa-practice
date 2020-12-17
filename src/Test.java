c51bebdacb5cc08673d2b13e12c3c6e25f7cc8cd
Jesus Rogelio Garza <jesusgarza2511@gmail.com>
2020-12-17 05:57:49 -0600

    Completed my solution for the Longest Common Prefix problem. Need to improve the solution.

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,16 @@
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(twoSum(new int[] {2,7,11,15}, 9)));
-        System.out.println(Arrays.toString(twoSum(new int[] {3, 2, 4}, 6)));
-        System.out.println(Arrays.toString(twoSum(new int[] {3, 3}, 6)));
+//        System.out.println(Arrays.toString(twoSum(new int[] {2,7,11,15}, 9)));
+//        System.out.println(Arrays.toString(twoSum(new int[] {3, 2, 4}, 6)));
+//        System.out.println(Arrays.toString(twoSum(new int[] {3, 3}, 6)));
+
+        System.out.println(longestCommonPrefix(new String[] { "flower","flow","flight" }));
+        System.out.println(longestCommonPrefix(new String[] { "dog","racecar","car" }));
+        System.out.println(longestCommonPrefix(new String[] { "hello","hell","helenor" }));
+        System.out.println(longestCommonPrefix(new String[] { }));
+        System.out.println(longestCommonPrefix(new String[] { "a" }));
     }
 
     // Two Sum
@@ -25,5 +30,48 @@ public class Test {
             myMap.put(target-nums[i], i); // store complement as key, index as value
         }
         return result;
+    }
+
+    // Longest Common Prefix
+    // Write a function to find the longest common prefix string amongst an array of strings.
+    // If there is no common prefix, return an empty string "".
+    public static String longestCommonPrefix(String[] strs) {
+        try {
+            if (strs.length > 200 || strs[0].length() > 200) {
+                return "";
+            }
+
+            String s = strs[0];
+            int min = strs[0].length();
+
+            // find the smallest word
+            for(int i = 1; i < strs.length; i++) {
+                if (min > strs[i].length()) {
+                    min = strs[i].length();
+                    s = strs[i];
+                }
+            }
+
+            // check if the letters of the smallest string are contained within all words
+            StringBuilder prefix = new StringBuilder();
+            boolean flag = true;
+            for(int i = 0; i < s.length(); i++) {
+                for(int j = 0; j < strs.length; j++) {
+                    if(s.charAt(i) != strs[j].charAt(i) && i == 0) {
+                        return "";
+                    } else if(s.charAt(i) != strs[j].charAt(i)) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    prefix.append(s.charAt(i));
+                }
+            }
+
+            return prefix.toString();
+        } catch(Exception e) {
+            return "";
+        }
     }
 }
