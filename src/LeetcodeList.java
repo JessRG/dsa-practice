79eeb75e7c544dee4c266f183af64dbe7a01b3e0
Jesus Rogelio Garza <jesusgarza2511@gmail.com>
2020-12-19 17:29:56 -0600

    Improved the solution of the Remove Duplicates from Sorted Array.

--- src/LeetcodeList.java
+++ src/LeetcodeList.java
@@ -121,28 +121,46 @@ public class LeetcodeList {
     // Given a sorted array nums, remove the duplicates in-place such that each element appears only once and returns the new length.
     // Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
     public int removeDuplicates(int[] nums) {
-        // Declare a hashset will use this to get rid of duplicates from the array nums
-        Set<Integer> set = new HashSet<>();
+//        // Declare a hashset will use this to get rid of duplicates from the array nums
+//        Set<Integer> set = new HashSet<>();
+//
+//        // loop through nums array and store each element into the hashset (autoboxing happens here)
+//        for(int i = 0; i < nums.length; i++) {
+//            set.add(nums[i]);
+//        }
+//
+//        // loop through hashset to store the values into an ArrayList
+//        Iterator it = set.iterator();
+//        ArrayList<Integer> unique = new ArrayList<>();
+//        while(it.hasNext()) {
+//            unique.add(Integer.parseInt(it.next().toString()));
+//        }
+//        // sort the unique list of values
+//        Collections.sort(unique);
+//
+//        // now store into the front of nums array
+//        for(int i = 0; i < unique.size(); i++) {
+//            nums[i] = unique.get(i);
+//        }
+//
+//        return unique.size();
 
-        // loop through nums array and store each element into the hashset (autoboxing happens here)
-        for(int i = 0; i < nums.length; i++) {
-            set.add(nums[i]);
+        // Improved solution
+        if (nums.length == 0) return 0;
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if(nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+
+            }
         }
-
-        // loop through hashset to store the values into an ArrayList
-        Iterator it = set.iterator();
-        ArrayList<Integer> unique = new ArrayList<>();
-        while(it.hasNext()) {
-            unique.add(Integer.parseInt(it.next().toString()));
-        }
-        // sort the unique list of values
-        Collections.sort(unique);
-
-        // now store into the front of nums array
-        for(int i = 0; i < unique.size(); i++) {
-            nums[i] = unique.get(i);
-        }
-
-        return unique.size();
+        return i + 1;
     }
+
+    // Implement strStr()
+    // Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+//    public int strStr(String haystack, String needle) {
+//
+//    }
 }
