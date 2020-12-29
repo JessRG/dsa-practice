79a6539c92b8a0ab3111043367830a545793ad94
Jesus Rogelio Garza <jesusgarza2511@gmail.com>
2020-12-28 23:57:57 -0600

    Just rearranged my solution methods position on the file to keep leetcode solutions above.

--- src/LeetcodeList.java
+++ src/LeetcodeList.java
@@ -487,6 +487,32 @@ public class LeetcodeList {
         System.out.println(Arrays.toString(s));
     }
 
+    // First Unique Character in a String
+    // Given a string, find the first non-repeating character in it and return its index. If it doesn't exist, return -1.
+    public int firstUniqChar(String s) {
+        // index local variable to store the index of the unique character
+        int idx = -1;
+        // Declare a hashmap to help solve this problem
+        HashMap<Character, Integer> hmap = new HashMap<>();
+
+        // build hashmap by storing each character along with a count of that character within the string
+        for(int i = 0; i < s.length();i++) {
+            hmap.put(s.charAt(i), hmap.getOrDefault(s.charAt(i), 0) + 1);
+        }
+
+        // loop through the string again to check if the character count is equal to 1 (unique)
+        for(int i = 0; i < s.length();i++) {
+            // check if character is unique
+            if (hmap.get(s.charAt(i)) == 1) {
+                // assign index of the current character then break the loop
+                idx = i;
+                break;
+            }
+        }
+        // return local idx variable
+        return idx;
+    }
+
     // Two Strings
     // Given two arrays of strings, determine whether corresponding elements contain a common substring.
     // For each test, print the result on a new line, either YES if there is a common substring, or NO.
@@ -571,32 +597,6 @@ public class LeetcodeList {
         if(bw != null) {
             bw.close();
         }
-    }
-
-    // First Unique Character in a String
-    // Given a string, find the first non-repeating character in it and return its index. If it doesn't exist, return -1.
-    public int firstUniqChar(String s) {
-        // index local variable to store the index of the unique character
-        int idx = -1;
-        // Declare a hashmap to help solve this problem
-        HashMap<Character, Integer> hmap = new HashMap<>();
-
-        // build hashmap by storing each character along with a count of that character within the string
-        for(int i = 0; i < s.length();i++) {
-            hmap.put(s.charAt(i), hmap.getOrDefault(s.charAt(i), 0) + 1);
-        }
-
-        // loop through the string again to check if the character count is equal to 1 (unique)
-        for(int i = 0; i < s.length();i++) {
-            // check if character is unique
-            if (hmap.get(s.charAt(i)) == 1) {
-                // assign index of the current character then break the loop
-                idx = i;
-                break;
-            }
-        }
-        // return local idx variable
-        return idx;
     }
 
     // Shopper's Delight
