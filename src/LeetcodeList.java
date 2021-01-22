76408c9c517b8ca08e00fc5f8a2e8c516ef63268
Jesus Rogelio Garza <jesusgarza2511@gmail.com>
2021-01-21 23:49:16 -0600

    Added the method signature for the Majority Element problem. Solution in progress...

--- src/LeetcodeList.java
+++ src/LeetcodeList.java
@@ -1180,5 +1180,12 @@ public class LeetcodeList {
         }
         return intersect;
     }
+
+    // Majority Element
+    // Given an array nums of size n, return the majority element.
+    // The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+    public int majorityElement(int[] nums) {
+        return 0;
+    }
 }
 
