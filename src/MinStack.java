0605f533b929165b21e3d3b2b183da725acc3720
Jesus Rogelio Garza <jesusgarza2511@gmail.com>
2021-02-05 23:53:12 -0600

    Initialize the instance variable of MinStack class with no-arg constructor. Define the logic of the push operation.

--- src/MinStack.java
+++ src/MinStack.java
@@ -1,4 +1,7 @@
-    // Min Stack
+import java.util.ArrayList;
+import java.util.List;
+
+// Min Stack
     //Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
     // push(x) -- Push element x onto stack.
     // pop() -- Removes the element on top of the stack.
@@ -7,12 +10,26 @@
 public class MinStack {
 
     /** initialize your data structure here. */
+    int lowIdx = -1;
+    // list to store all/any values entered into the minStack
+    List<Integer> list;
+    // List to store the indexes of the most minimum value at the end (tail) of the list
+    List<Integer> lowest;
+
     public MinStack() {
-        // Write logic for MinStack here...
+        // Initialize the instance variables (Lists/ArrayLists)
+        list = new ArrayList<>();
+        lowest = new ArrayList<>();
     }
 
     public void push(int x) {
-        // Write logic for MinStack here...
+        // add parameter (x) into the list and increment the lowIdx instance variable
+        list.add(x);
+        lowIdx++;
+        // push into the lowest list if the list is currently empty
+        if(lowest.isEmpty()) lowest.add(lowIdx);
+        // if the parameter is less than or equal to the most minimum value
+        else if(list.get(lowest.get(lowest.size() - 1)) >= x) lowest.add(lowIdx);
     }
 
     public void pop() {
