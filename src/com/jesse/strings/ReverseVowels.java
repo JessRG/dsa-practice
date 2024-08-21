78f2e625b55a804afd75a46410d85fa9ab144e12
Jesus Garza <jesusgarza2511@gmail.com>
2024-08-21 18:15:13 -0500

    Add ReverseVowels problem and solution

---
+++ src/com/jesse/strings/ReverseVowels.java
@@ -0,0 +1,38 @@
+package com.jesse.strings;
+
+public class ReverseVowels {
+    public static void main(String[] args) {
+//        String s = "hello";
+        String s = "AEIOU";
+//        String s = "DesignGUrus";
+        System.out.print(reverseVowels(s));
+    }
+
+    public static String reverseVowels(String s) {
+        // TODO: Write your code here
+        int start = 0;
+        int end = s.length() - 1;
+
+        String vowels = "aeiouAEIOU";
+        char[] ch = s.toCharArray();
+        while (start < end) {
+            while (vowels.indexOf(ch[start]) == -1) {
+                start++;
+            }
+            while (vowels.indexOf(ch[end]) == -1) {
+                end--;
+            }
+
+            if (start < end) {
+                // swap
+                char temp = ch[start];
+                ch[start] = ch[end];
+                ch[end] = temp;
+                start++;
+                end--;
+            }
+        }
+        s = new String(ch);
+        return s;
+    }
+}
