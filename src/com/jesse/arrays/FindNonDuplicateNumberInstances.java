a43c99dad242e57fb064ffd8ace076d90feb5cf2
Jesus Garza <jesusgarza2511@gmail.com>
2024-08-23 17:35:43 -0500

    Find Non-Duplicate Number Instances: add alternative solution

--- src/com/jesse/arrays/FindNonDuplicateNumberInstances.java
+++ src/com/jesse/arrays/FindNonDuplicateNumberInstances.java
@@ -8,31 +8,46 @@ public class FindNonDuplicateNumberInstances {
         System.out.println(moveElements(arr));
     }
 
-    public static int moveElements(int[] arr) {
-        // TODO: Write your code here
-        int start = 0;
-        int end = start + 1;
+//    public static int moveElements(int[] arr) {
+//        // TODO: Write your code here
+//        int start = 0;
+//        int end = start + 1;
+//
+//        while (end < arr.length) {
+//            if (arr[start] != arr[end]) {
+//                if (end - start > 1) {
+//                    start++;
+//                    swap(arr, start, end);
+//                    end++;
+//                } else {
+//                    start++;
+//                    end++;
+//                }
+//            } else {
+//                end++;
+//            }
+//        }
+//        return start+1;
+//    }
+//
+//    private static void swap(int[] a, int s, int e) {
+//        int temp = a[s];
+//        a[s] = a[e];
+//        a[e] = temp;
+//    }
 
-        while (end < arr.length) {
-            if (arr[start] != arr[end]) {
-                if (end - start > 1) {
-                    start++;
-                    swap(arr, start, end);
-                    end++;
-                } else {
-                    start++;
-                    end++;
-                }
-            } else {
-                end++;
+    // Alternate solution
+    public static int moveElements(int[] arr) {
+        int nextNonDuplicate = 1;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[nextNonDuplicate - 1] != arr[i]) {
+                arr[nextNonDuplicate] = arr[i];
+                nextNonDuplicate++;
             }
         }
-        return start+1;
-    }
 
-    private static void swap(int[] a, int s, int e) {
-        int temp = a[s];
-        a[s] = a[e];
-        a[e] = temp;
+        // Return the length of the modified array (number of non-duplicate elements)
+        return nextNonDuplicate;
     }
 }
