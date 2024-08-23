5faa7bc69adb22edd55535a66900afb1f141db47
Jesus Garza <jesusgarza2511@gmail.com>
2024-08-23 16:04:58 -0500

    Pair with Target Sum: Add alternative solution

--- src/com/jesse/arrays/PairWithTargetSum.java
+++ src/com/jesse/arrays/PairWithTargetSum.java
@@ -1,6 +1,21 @@
 package com.jesse.arrays;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class PairWithTargetSum {
+    public static void main(String[] args) {
+//        int[] arr = new int[] {1, 2, 3, 4, 6};
+//        int target = 6;
+        int[] arr = new int[] {2, 5, 9, 11};
+        int target = 11;
+//        int[] arr = new int[] {1, 3, 5, 7, 9};
+//        int target = 16;
+
+        System.out.println(Arrays.toString(search(arr, target)));
+        System.out.println(Arrays.toString(searchWithMap(arr, target)));
+    }
+
     public static int[] search(int[] arr, int targetSum) {
         // TODO: Write your code here
         int[] res = new int[] {-1, -1};
@@ -25,5 +40,18 @@ public class PairWithTargetSum {
             }
         }
         return res;
+    }
+
+    public static int[] searchWithMap(int[] arr, int targetSum) {
+        // TODO: Write your code here
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (map.containsKey(targetSum - arr[i])) {
+                return new int[] { map.get(targetSum - arr[i]), i };
+            } else {
+                map.put(arr[i], i);
+            }
+        }
+        return new int[] { -1, -1 };
     }
 }
