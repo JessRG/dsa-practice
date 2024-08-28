3498e10967eb26cf72ed3c61eea3fcf20a8641bf
Jesus Garza <jesusgarza2511@gmail.com>
2024-08-28 11:37:50 -0500

    Find If Path Exists: Add dfs alternative solution

--- src/com/jesse/graphs/FindIfPathExists.java
+++ src/com/jesse/graphs/FindIfPathExists.java
@@ -15,46 +15,77 @@ public class FindIfPathExists {
 //        start = 0;
 //        end = 2;
 
-        n = 5;
-        edges = new int[][] {{0,1},{1,2},{2,3},{3,4}};
-        start = 0;
-        end = 4;
+//        n = 5;
+//        edges = new int[][] {{0,1},{1,2},{2,3},{3,4}};
+//        start = 0;
+//        end = 4;
 
         System.out.println(validPath(n, edges, start, end));
     }
 
+//    public static boolean validPath(int n, int[][] edges, int start, int end) {
+//        // ToDo: Write Your Code Here.
+//        HashMap<Integer, List<Integer>> adj = new HashMap<>();
+//        for (int i = 0; i < n; i++) {
+//            adj.put(i, new ArrayList<>());
+//        }
+//        for (int[] edge : edges) {
+//            int n1 = edge[0];
+//            int n2 = edge[1];
+//            adj.get(n1).add(n2);
+//            adj.get(n2).add(n1);
+//        }
+//
+//        visited = new boolean[n];
+//        Queue<Integer> q = new LinkedList<>();
+//        q.add(start);
+//
+//        while (!q.isEmpty()) {
+//            int currentVertex = q.poll();
+//
+//            if (currentVertex == end) {
+//                return true;
+//            }
+//
+//            for(int neighbor : adj.get(currentVertex)) {
+//                if (!visited[neighbor]) {
+//                    visited[currentVertex] = true;
+//                    q.add(neighbor);
+//                }
+//            }
+//        }
+//
+//        return false;
+//    }
+
     public static boolean validPath(int n, int[][] edges, int start, int end) {
-        // ToDo: Write Your Code Here.
-        HashMap<Integer, List<Integer>> adj = new HashMap<>();
+        List<List<Integer>> graph = new ArrayList<>();
+
+        // Initialize the graph
         for (int i = 0; i < n; i++) {
-            adj.put(i, new ArrayList<>());
+            graph.add(new ArrayList<>());
         }
+
+        // Populate the graph from edges
         for (int[] edge : edges) {
-            int n1 = edge[0];
-            int n2 = edge[1];
-            adj.get(n1).add(n2);
-            adj.get(n2).add(n1);
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]); // Because it's an undirected graph
         }
 
         visited = new boolean[n];
-        Queue<Integer> q = new LinkedList<>();
-        q.add(start);
+        return dfs(graph, start, end);
+    }
 
-        while (!q.isEmpty()) {
-            int currentVertex = q.poll();
+    private static boolean dfs(List<List<Integer>> graph, int node, int end) {
+        if (node == end) return true; // Found the path
+        visited[node] = true;
 
-            if (currentVertex == end) {
+        // Traverse neighbors
+        for (int neighbor : graph.get(node)) {
+            if (!visited[neighbor] && dfs(graph, neighbor, end)) {
                 return true;
             }
-
-            for(int neighbor : adj.get(currentVertex)) {
-                if (!visited[neighbor]) {
-                    visited[currentVertex] = true;
-                    q.add(neighbor);
-                }
-            }
         }
-
-        return false;
+        return false; // Path not found
     }
 }
