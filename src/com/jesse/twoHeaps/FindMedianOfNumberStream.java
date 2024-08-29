23902348a5cfb51b1fc276e4618f4aa386b2e096
Jesus Garza <jesusgarza2511@gmail.com>
2024-08-29 12:27:05 -0500

    Add Find Median Of Number Stream problem and solution

--- src/com/jesse/twoHeaps/FindMedianOfNumberStream.java
+++ src/com/jesse/twoHeaps/FindMedianOfNumberStream.java
@@ -5,44 +5,74 @@ import java.util.PriorityQueue;
 public class FindMedianOfNumberStream {
     PriorityQueue<Integer> maxHeap;
     PriorityQueue<Integer> minHeap;
-    int size;
+//    int size;
+//    public FindMedianOfNumberStream() {
+//        //TODO: Write your code here
+//        maxHeap = new PriorityQueue<>((a, b) -> b - a);
+//        minHeap = new PriorityQueue<>();
+//        size = 0;
+//    }
+//
+//    public void insertNum(int num) {
+//        // TODO: Write your code here
+//        if (!minHeap.isEmpty()) {
+//            while (!minHeap.isEmpty()) {
+//                Integer elem = minHeap.remove();
+//                maxHeap.add(elem);
+//            }
+//        }
+//        maxHeap.add(num);
+//        size++;
+//    }
+//
+//    public double findMedian() {
+//        // TODO: Write your code here
+//        try {
+//            int len = size / 2;
+//            for (int i = 0; i < len; i++) {
+//                Integer elem = maxHeap.remove();
+//                minHeap.add(elem);
+//            }
+//            if (size % 2 == 0) {
+//                Integer min = minHeap.peek();
+//                Integer max = maxHeap.peek();
+//                return ((double) (min + max) / 2);
+//            } else {
+//                return (double) maxHeap.peek();
+//            }
+//        } catch (NullPointerException ex) {
+//            throw new NullPointerException("Peek operation is returning null");
+//        }
+//    }
+
+    // Improved solution
     public FindMedianOfNumberStream() {
         //TODO: Write your code here
         maxHeap = new PriorityQueue<>((a, b) -> b - a);
         minHeap = new PriorityQueue<>();
-        size = 0;
     }
 
     public void insertNum(int num) {
-        // TODO: Write your code here
-        if (!minHeap.isEmpty()) {
-            while (!minHeap.isEmpty()) {
-                Integer elem = minHeap.remove();
-                maxHeap.add(elem);
-            }
-        }
-        maxHeap.add(num);
-        size++;
+        if (maxHeap.isEmpty() || maxHeap.peek() >= num)
+            maxHeap.add(num);
+        else
+            minHeap.add(num);
+
+        // either both the heaps will have equal number of elements or max-heap will have one
+        // more element than the min-heap
+        if (maxHeap.size() > minHeap.size() + 1)
+            minHeap.add(maxHeap.poll());
+        else if (maxHeap.size() < minHeap.size())
+            maxHeap.add(minHeap.poll());
     }
 
     public double findMedian() {
-        // TODO: Write your code here
-        try {
-            int len = size / 2;
-            for (int i = 0; i < len; i++) {
-                Integer elem = maxHeap.remove();
-                minHeap.add(elem);
-            }
-            if (size % 2 == 0) {
-                Integer min = minHeap.peek();
-                Integer max = maxHeap.peek();
-                return ((double) (min + max) / 2);
-            } else {
-                return (double) maxHeap.peek();
-            }
-        } catch (NullPointerException ex) {
-            throw new NullPointerException("Peek operation is returning null");
+        if (maxHeap.size() == minHeap.size()) {
+            // we have even number of elements, take the average of middle two elements
+            return maxHeap.peek() / 2.0 + minHeap.peek() / 2.0;
         }
+        // because max-heap will have one more element than the min-heap
+        return maxHeap.peek();
     }
 
     public static void main(String[] args) {
