6938ce6b76134fa12801842804a5dee1635cf54b
Jesus Garza <jesusgarza2511@gmail.com>
2024-08-30 12:13:24 -0500

    Islands: Add BFS solution

--- src/com/jesse/graphs/Islands.java
+++ src/com/jesse/graphs/Islands.java
@@ -1,5 +1,8 @@
 package com.jesse.graphs;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Islands {
     public static void main(String[] args) {
         Islands islands = new Islands();
@@ -61,7 +64,8 @@ public class Islands {
                 if (matrix[i][j] == 1) { // only if the cell is a land
                     // we have found an island
                     totalIslands++;
-                    visitIslandDFS(matrix, i, j);
+//                    visitIslandDFS(matrix, i, j);
+                    visitIslandBFS(matrix, i, j);
                 }
             }
         }
@@ -81,5 +85,28 @@ public class Islands {
         visitIslandDFS(matrix, x - 1, y); // upper cell
         visitIslandDFS(matrix, x, y + 1); // right cell
         visitIslandDFS(matrix, x, y - 1); // left cell
+    }
+
+    private static void visitIslandBFS(int[][] matrix, int x, int y) {
+        Queue<int[]> neighbors = new LinkedList<>();
+        neighbors.add(new int[] { x, y });
+        while (!neighbors.isEmpty()) {
+            int row = neighbors.peek()[0];
+            int col = neighbors.peek()[1];
+            neighbors.remove();
+
+            if (row < 0 || row >= matrix.length || col < 0 || col >= matrix[0].length)
+                continue; // continue, if it is not a valid cell
+            if (matrix[row][col] == 0)
+                continue; // continue if it is a water cell
+
+            matrix[row][col] = 0; // mark the cell visited by making it a water cell
+
+            // insert all neighboring cells to the queue for BFS
+            neighbors.add(new int[] { row + 1, col }); // lower cell
+            neighbors.add(new int[] { row - 1, col }); // upper cell
+            neighbors.add(new int[] { row, col + 1 }); // right cell
+            neighbors.add(new int[] { row, col - 1 }); // left cell
+        }
     }
 }
