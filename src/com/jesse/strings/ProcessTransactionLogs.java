7a60b89a24bcdb0d5ad1ca78eba1ffdd77891234
Jesus Garza <jesusgarza2511@gmail.com>
2024-08-30 19:39:01 -0500

    Process Transaction Logs: refactor code in for loop

--- src/com/jesse/strings/ProcessTransactionLogs.java
+++ src/com/jesse/strings/ProcessTransactionLogs.java
@@ -6,44 +6,24 @@ public class ProcessTransactionLogs {
     public static List<String> processLogs(List<String> logs, int threshold) {
         // Write your code here
         List<String> res = new ArrayList<>();
-        Map<String, Integer> transactions = new HashMap<>();
+        Map<String, Integer> tmap = new HashMap<>();
 
         for (String log : logs) {
             String[] split = log.split(" ");
             String senderId = split[0];
             String recipientId = split[1];
 
-            if (senderId.equalsIgnoreCase(recipientId)) {
-                if (transactions.containsKey(senderId)) {
-                    transactions.put(senderId, transactions.get(senderId) + 1);
-                } else {
-                    transactions.put(senderId, 1);
-                }
-            } else {
-                if (transactions.containsKey(senderId)) {
-                    transactions.put(senderId, transactions.get(senderId) + 1);
-                } else {
-                    transactions.put(senderId, 1);
-                }
-                if (transactions.containsKey(recipientId)) {
-                    transactions.put(recipientId, transactions.get(recipientId) + 1);
-                } else {
-                    transactions.put(recipientId, 1);
-                }
-            }
+            tmap.put(senderId, tmap.getOrDefault(senderId, 0) + 1);
+            if (!senderId.equals(recipientId)) tmap.put(recipientId, tmap.getOrDefault(recipientId, 0) + 1);
         }
 
-        for (Map.Entry<String, Integer> transaction : transactions.entrySet()) {
+        for (Map.Entry<String, Integer> transaction : tmap.entrySet()) {
             if (transaction.getValue() >= threshold) {
                 res.add(transaction.getKey());
             }
         }
 
-        res.sort(new Comparator<String>() {
-            public int compare(String a, String b) {
-                return Long.compare(Long.parseLong(a), Long.parseLong(b));
-            }
-        });
+        res.sort((a, b) -> Long.compare(Long.parseLong(a), Long.parseLong(b)));
 
         return res;
     }
