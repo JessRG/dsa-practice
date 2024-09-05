9e261f17cd871f6ea6c8d48fa3061dc8206fe11c
Jesus Garza <jesusgarza2511@gmail.com>
2024-09-05 11:41:05 -0500

    Add 0/1 Knapsack problem and recursive solution

--- src/com/jesse/dp/Knapsack.java
+++ src/com/jesse/dp/Knapsack.java
@@ -6,13 +6,28 @@ public class Knapsack {
         int[] profits = new int[] {1,6,10,16};
         int[] weights = new int[] {1,2,3,5};
         int capacity = 7;
-        profits = new int[] {1,6,10,16};
-        weights = new int[] {1,2,3,5};
-        capacity = 6;
+//        profits = new int[] {1,6,10,16};
+//        weights = new int[] {1,2,3,5};
+//        capacity = 6;
         System.out.println(ks.solveKnapsack(profits, weights, capacity));
     }
     public int solveKnapsack(int[] profits, int[] weights, int capacity) {
         // TODO: Write your code here
-        return -1;
+        return knapsackRecursive(profits, weights, capacity, 0);
+    }
+
+    private int knapsackRecursive(int[] profits, int[] weights, int capacity, int index) {
+        if (index >= profits.length) {
+            return 0;
+        }
+
+        int profit1 = Integer.MIN_VALUE;
+        if (weights[index] <= capacity) {
+            profit1 = profits[index] + knapsackRecursive(profits, weights, capacity - weights[index], index + 1);
+        }
+
+        int profit2 = knapsackRecursive(profits, weights, capacity, index + 1);
+
+        return Math.max(profit1, profit2);
     }
 }
