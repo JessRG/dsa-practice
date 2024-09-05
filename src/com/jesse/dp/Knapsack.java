b05906363d9d3318be95d43e53ed921ecff7a536
Jesus Garza <jesusgarza2511@gmail.com>
2024-09-05 17:22:22 -0500

    0/1 Knapsack: Add tabulation solution

--- src/com/jesse/dp/Knapsack.java
+++ src/com/jesse/dp/Knapsack.java
@@ -10,6 +10,7 @@ public class Knapsack {
 //        weights = new int[] {1,2,3,5};
 //        capacity = 6;
         System.out.println(ks.solveKnapsack(profits, weights, capacity));
+        System.out.println(ks.solveKnapsackTabulation(profits, weights, capacity));
     }
 //    public int solveKnapsack(int[] profits, int[] weights, int capacity) {
 //        // TODO: Write your code here
@@ -56,5 +57,44 @@ public class Knapsack {
 
         dp[index][capacity] = Math.max(profits1, profits2);
         return dp[index][capacity];
+    }
+
+    // ** Tabulation solution **
+    public int solveKnapsackTabulation(int[] profits, int[] weights, int capacity) {
+        // basic checks
+        if (capacity == 0 || profits.length == 0 || weights.length != profits.length) {
+            return 0;
+        }
+
+        int n = profits.length;
+        int[][] dp = new int[n][capacity + 1];
+
+        // populate the capacity=0 columns, with '0' capacity we have '0' profit
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = 0;
+        }
+
+        // if we have only one weight, we will take it if it is not more than the capacity
+        for (int c = 0; c <= capacity; c++) {
+            if (weights[0] <= c) {
+                dp[0][c] = profits[0];
+            }
+        }
+
+        // process all sub-arrays for all the
+        for (int i = 1; i < n; i++) {
+            for (int c = 1; c <= capacity; c++) {
+                int profit1 = 0, profit2;
+                // include item if it is not greater than the capacity
+                if (weights[i] <= c) {
+                    profit1 = profits[i] + dp[i - 1][c - weights[i]];
+                }
+                profit2 = dp[i-1][c];
+
+                dp[i][c] = Math.max(profit1, profit2);
+            }
+        }
+
+        return dp[n-1][capacity];
     }
 }
