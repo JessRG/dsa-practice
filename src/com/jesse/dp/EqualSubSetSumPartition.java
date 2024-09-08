ac2effbf30f600a7c7833edbaff5ffc646931752
Jesus Garza <jesusgarza2511@gmail.com>
2024-09-08 00:31:13 -0500

    Equal Subset Sum Partition: Add Bottom-up (Tabulation) solution

--- src/com/jesse/dp/EqualSubSetSumPartition.java
+++ src/com/jesse/dp/EqualSubSetSumPartition.java
@@ -6,6 +6,7 @@ public class EqualSubSetSumPartition {
 //        num = new int[] {1, 1, 3, 4, 7};
 //        num = new int[] {2, 3, 4, 6};
         System.out.println(canPartition(num));
+        System.out.println(canPartitionTabulation(num));
     }
 
 //    public static boolean canPartition(int[] num) {
@@ -84,5 +85,53 @@ public class EqualSubSetSumPartition {
         dp[index][sum] = partition(dp, num, sum, index + 1);
 
         return dp[index][sum];
+    }
+
+    // ** Bottom-up Approach (Tabulation) **
+    public static boolean canPartitionTabulation(int[] num) {
+        //TODO: Write - Your - Code
+        if (num.length == 0) {
+            return false;
+        }
+        int n = num.length;
+
+        // calculate the total sum
+        int totalSum = 0;
+        for (int val : num) {
+            totalSum += val;
+        }
+
+        if (totalSum % 2 != 0) {
+            return false;
+        }
+        int sum = totalSum / 2;
+        Boolean[][] dp = new Boolean[n][sum + 1];
+
+        // populate the sum=0 columns, as we can always for '0' sum with an empty set
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = true;
+        }
+
+        // with only one number, we can form a subset only when the required sum is equal to
+        // its value
+        for (int s = 1; s <= sum; s++) {
+            dp[0][s] = s == num[0];
+        }
+
+        // process all subsets
+        for (int i = 1; i < n; i++) {
+            for (int s = 1; s <= sum; s++) {
+                // if we can get the sum 's' without the number at index 'i'
+                if (dp[i-1][s]) {
+                    dp[i][s] = true;
+                } else if (s >= num[i]) {
+                    // else we can find a subset to get the remaining sum
+                    dp[i][s] = dp[i-1][s - num[i]];
+                }
+            }
+        }
+
+        // bottom right corner will be our answer
+        return dp[n-1][sum];
     }
 }
