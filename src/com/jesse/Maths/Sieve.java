f270a3c1186a925e27b5cd8668346de8c7342242
Jesus Garza <jesusgarza2511@gmail.com>
2024-09-19 16:08:33 -0500

    Add Sieve of Eratosthenes to Maths package

---
+++ src/com/jesse/Maths/Sieve.java
@@ -0,0 +1,26 @@
+package com.jesse.Maths;
+
+public class Sieve {
+    // Sieve of Eratosthenes
+    public static void main(String[] args) {
+        int n = 40;
+        boolean[] primes = new boolean[n + 1];
+        sieve(n, primes);
+    }
+
+    private static void sieve(int n, boolean[] primes) {
+        for (int i = 2; i * i <= n; i++) {
+            if (!primes[i]) {
+                for (int j = i*2; j <= n; j+=i) {
+                    primes[j] = true;
+                }
+            }
+        }
+
+        for (int i = 2; i <= n; i++) {
+            if (!primes[i]) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
