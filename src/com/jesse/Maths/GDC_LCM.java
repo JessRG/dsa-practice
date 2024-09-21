bdb24f5e4882b53f8f7ea24ff36fda4775499a9f
Jesus Garza <jesusgarza2511@gmail.com>
2024-09-21 10:49:39 -0500

    Add Extra Characters in a String problem and solution

--- src/com/jesse/Maths/GDC_LCM.java
+++ src/com/jesse/Maths/GDC_LCM.java
@@ -2,7 +2,8 @@ package com.jesse.Maths;
 
 public class GDC_LCM {
     public static void main(String[] args) {
-        System.out.println(gcd(4, 9));
+//        System.out.println(gcd(4, 9));
+        System.out.println(lcm(2, 7));
     }
 
     // Euclidean algorithm
@@ -12,5 +13,10 @@ public class GDC_LCM {
         }
 
         return gcd(b%a, a);
+    }
+
+    // Least common multiple
+    static int lcm(int a, int b) {
+        return a * b / gcd(a,b);
     }
 }
---
+++ src/com/jesse/Trie/ExtraCharacters.java
@@ -0,0 +1,54 @@
+package com.jesse.Trie;
+
+// Extra Characters in a String
+public class ExtraCharacters {
+    public static void main(String[] args) {
+        String[] dictionary = new String[] {"program", "test", "java", "python"};
+        String word = "programmingtest";
+//        word = "javaandpython";
+        System.out.println(minExtraChars(dictionary, word));
+    }
+
+    private static int minExtraChars(String[] dictionary, String text) {
+        // build trie from dictionary
+        int n = text.length();
+        TrieNode root = buildTrie(dictionary);
+        int[] dp = new int[n+1]; // dp array stores minimum extra characters
+
+        // calculate dp array; iterate over text string in reverse
+        for (int s = n - 1; s >= 0; s--) {
+            dp[s] = dp[s + 1] + 1; // Default case: considering current character as extra.
+
+            TrieNode node = root;
+            for (int e = s; e < text.length(); e++) {
+                char c = text.charAt(e);
+                if (node.children[c - 'a'] == null) {
+                    break;
+                }
+                node = node.children[c - 'a'];
+
+                if (node.isEndOfWord) {
+                    // update dp if word is found
+                    dp[s] = Math.min(dp[s], dp[e+1]);
+                }
+            }
+        }
+
+        return dp[0];
+    }
+
+    private static TrieNode buildTrie(String[] dictionary) {
+        TrieNode root = new TrieNode();
+        for (String word : dictionary) {
+            TrieNode node = root;
+            for (char c : word.toCharArray()) {
+                if (node.children[c - 'a'] == null) {
+                    node.children[c - 'a'] = new TrieNode();
+                }
+                node = node.children[c - 'a'];
+            }
+            node.isEndOfWord = true;
+        }
+        return root;
+    }
+}
